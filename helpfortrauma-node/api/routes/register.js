16710fbcdeebfd8afaa042af7412eb07816b9ce3
Vinay <vinaypatidar.linuxbean@gmail.com>
2018-08-10 18:49:56 +0530

    updated user email validation in register

--- helpfortrauma-node/api/routes/register.js
+++ helpfortrauma-node/api/routes/register.js
@@ -40,29 +40,44 @@ router.post('/user', (req, res, next) => {
 
 	}
 	else {
-        const user = new Register({
-            fName: req.body.fName,
-            lName: req.body.lName,
-            email: req.body.email,
-            profession: req.body.profession,
-            address: req.body.address,
-            address2: req.body.address2,
-            city: req.body.city,
-            state: req.body.state,
-            zip: req.body.zip,
-            username: req.body.username,
-            pass: req.body.pass,
-            created: new Date()
+        Register.findOne({ email: req.body.email})
+        .then( data => {
+            if(data) {
+                res.status(201).json({
+                    message: "User already registerd try with another email",
+                });
+            } else {
+                const user = new Register({
+                    fName: req.body.fName,
+                    lName: req.body.lName,
+                    email: req.body.email,
+                    profession: req.body.profession,
+                    address: req.body.address,
+                    address2: req.body.address2,
+                    city: req.body.city,
+                    state: req.body.state,
+                    zip: req.body.zip,
+                    username: req.body.username,
+                    pass: req.body.pass,
+                    created: new Date()
+                    });
+        
+                user.save()
+                    .then(data => {
+                        res.status(201).json({
+                            message: 'Data added successfully',
+                        });
+                    }).catch(err => {
+                        console.log(err);
+                    });
+            }
+        }).catch(err => {
+            res.status(400).json({
+                message: err,
             });
+        })
 
-		user.save()
-			.then(data => {
-				res.status(201).json({
-					message: 'Data added successfully',
-				});
-			}).catch(err => {
-				console.log(err);
-			});
+        
 	}
 });
 
