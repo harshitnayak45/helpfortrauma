7172679c6e7f0e207028a494b9f7f879930f84ae
raghav <raghavendra.linuxbean@gmail.com>
2018-08-10 16:05:58 +0530

    new

--- helpfortrauma-react/src/Register.js
+++ helpfortrauma-react/src/Register.js
@@ -1,7 +1,67 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery'; 
+import axios from 'axios';
 class Register extends Component {
- 
+
+	
+
+	constructor(props){
+    super(props);
+    this.state={
+    	_id:'',
+    	signupemail:'',
+    	signupfullname:'',
+    	signupusername:'',
+    	loginpassword:'',
+    	agree:''
+    };
+    this.RegisterMe=this.RegisterMe.bind(this);
+  }
+
+  RegisterMe(event){
+
+  	console.log('fdf');
+	// var signupemail=this.refs.signupemail.value;
+	// var signupfullname=this.refs.signupfullname.value;
+	// var signupusername=this.refs.signupusername.value;
+	// var loginpassword=this.refs.loginpassword.value;
+	// var agree=this.refs.agree.value;
+
+	 var studentdata = {  
+        'signupemail': this.refs.signupemail.value,  
+        'signupfullname':this.refs.signupfullname.value,
+        'signupusername':this.refs.signupusername.value,  
+        'loginpassword':this.refs.loginpassword.value,  
+        'agree':this.refs.agree.value  
+          
+    }  
+var Url="http://localhost:3300/register";
+     axios.get(Url)
+      .then(res => {
+      	console.log(res);
+        //this.setState({ books: res.data });
+        //console.log(this.state.books);
+      });
+
+
+
+
+
+
+
+	// this.setState({
+	// 	_id:'',
+	// 	signupemail:signupemail,
+	// 	signupfullname:signupfullname,
+	// 	signupusername:signupusername,
+	// 	loginpassword:loginpassword,
+	// 	agree:agree
+	// }, function(){
+	// 	console.log(this.state);
+ //    })
+
+  }
  
   render() {
     return (
@@ -25,36 +85,36 @@ class Register extends Component {
 
       <h5 className="account-body-subtitle">Sign up in 30 seconds. No credit card required.</h5>
 
-      <form className="form account-form" method="POST" action="./index.html">
+      <form className="form account-form"  action="#">
 
         <div className="form-group">
           <label htmlFor="signup-email" className="placeholder-hidden">Email Address</label>
-          <input type="text" className="form-control" id="signup-email" placeholder="Your Email" tabIndex="1" />
+          <input type="text" className="form-control" id="signupemail" placeholder="Your Email" ref="signupemail" tabIndex="1" />
         </div> 
 
         <div className="form-group">
           <label htmlFor="signup-fullname" className="placeholder-hidden">Your Name</label>
-          <input type="text" className="form-control" id="signup-fullname" placeholder="Your Name" tabIndex="2" />
+          <input type="text" className="form-control" id="signupfullname" ref="signupfullname" placeholder="Your Name" tabIndex="2" />
         </div> 
 
         <div className="form-group">
           <label htmlFor="signup-username" className="placeholder-hidden">Username</label>
-          <input type="text" className="form-control" id="signup-username" placeholder="Username" tabIndex="3" />
+          <input type="text" className="form-control" id="signupusername" ref="signupusername" placeholder="Username" tabIndex="3" />
         </div> 
 
         <div className="form-group">
           <label htmlFor="login-password" className="placeholder-hidden">Password</label>
-          <input type="password" className="form-control" id="login-password" placeholder="Password" tabIndex="4" />
+          <input type="password" className="form-control" id="login-password" ref="loginpassword" placeholder="Password" tabIndex="4" />
         </div> 
 
         <div className="form-group">
           <label className="checkbox-inline">
-          <input type="checkbox" className="" value="" tabIndex="5" /> I agree to the <a href="javascript:;" target="_blank">Terms of Service</a> &amp; <a href="javascript:;" target="_blank">Privacy Policy</a>
+          <input type="checkbox" className="" ref="agree" value="1" tabIndex="5" /> I agree to the <a href="javascript:;" target="_blank">Terms of Service</a> &amp; <a href="javascript:;" target="_blank">Privacy Policy</a>
           </label>
         </div> 
 
         <div className="form-group">
-          <button type="submit" className="btn btn-secondary btn-block btn-lg" tabIndex="6">
+          <button type="button" onClick={this.RegisterMe}  className="btn btn-secondary btn-block btn-lg" tabIndex="6">
           Create My Account &nbsp; <i className="fa fa-play-circle"></i>
           </button>
         </div> 
