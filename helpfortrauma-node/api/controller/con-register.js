688df27bc4e2b2770845ee00ca3580dc9b52b141
Vinay <vinaypatidar.linuxbean@gmail.com>
2018-08-11 13:04:41 +0530

    password encrypted with bycrypt encryption and updated controller

---
+++ helpfortrauma-node/api/controller/con-register.js
@@ -0,0 +1,77 @@
+const Register = require('../model/user-info.vo');
+const bcrypt = require('bcrypt');
+
+/* *******************************User Registration************************************* */
+exports.addUpdateUser = (req, res, next) => {
+    const reqObj = _setReqData(req);
+    if (req.body._id) {
+        User.findByIdAndUpdate(req.body._id, reqObj, { new: true })
+            .then(data => {
+                res.status(201).json({
+                    message: 'Data Updated successfully',
+                    success: true
+                });
+            }).catch(err => {
+                console.log(err);
+            });
+    }
+    else {
+        Register.findOne({ email: req.body.email })
+            .then(data => {
+                if (data) {
+                    res.status(201).json({
+                        message: "User already registerd try with another email",
+                        success: false
+                    });
+                } else {
+                    const user = new Register(reqObj);
+                    user.save()
+                        .then(data => {
+                            res.status(201).json({
+                                message: 'Data added successfully',
+                                success: true
+                            });
+                        }).catch(err => {
+                            console.log(err);
+                        });
+                }
+            }).catch(err => {
+                res.status(400).json({
+                    message: err,
+                });
+            })
+    }
+}
+
+/* ****************************Get all registered user**************************** */
+exports.getAllregisterUser = (req, res, next) => {
+    Register.find({}, (err, users) => {
+        res.status(200).json({
+            message: 'all registerd users',
+            success: true,
+            body: users
+        });
+    });
+}
+
+/* ****************************Private functions**************************** */
+function _setReqData(req) {
+    let hash = bcrypt.hashSync(req.body.pass, 10);
+    console.log(hash);
+    const reqObj = {
+        fName: req.body.fName,
+        lName: req.body.lName,
+        email: req.body.email,
+        cell: req.body.cell,
+        profession: req.body.profession,
+        address: req.body.address,
+        address2: req.body.address2,
+        city: req.body.city,
+        state: req.body.state,
+        zip: req.body.zip,
+        username: req.body.username,
+        pass: hash,
+        created: new Date()
+    }
+    return reqObj;
+}
--- helpfortrauma-node/api/routes/register.js
+++ helpfortrauma-node/api/routes/register.js
@@ -1,85 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const Register = require('../model/user-info.vo');
+const conRegister = require('../controller/con-register');
 
 /* ****************************Get all registered user**************************** */
-router.get('/user/all', (req, res, next) => {
-    Register.find({}, (err, users) => {
-		res.status(200).json({
-			message: 'get request for details',
-			body: users
-		});
-	});
-});
+router.get('/user/all', conRegister.getAllregisterUser);
 
 /* *******************************User Registration************************************* */
-router.post('/user', (req, res, next) => {
-	if (req.body._id) {
-		User.findByIdAndUpdate(req.body._id, {
-            fName: req.body.fName,
-            lName: req.body.lName,
-            email: req.body.email,
-            cell: req.body.cell,
-            profession: req.body.profession,
-            address: req.body.address,
-            address2: req.body.address2,
-            city: req.body.city,
-            state: req.body.state,
-            zip: req.body.zip,
-            username: req.body.username,
-            pass: req.body.pass,
-            created: new Date()
-		}, { new: true })
-			.then(data => {
-				res.status(201).json({
-					message: 'Data Updated successfully',
-				});
-			}).catch(err => {
-				console.log(err);
-			});
-
-	}
-	else {
-        Register.findOne({ email: req.body.email})
-        .then( data => {
-            if(data) {
-                res.status(201).json({
-                    message: "User already registerd try with another email",
-                });
-            } else {
-                const user = new Register({
-                    fName: req.body.fName,
-                    lName: req.body.lName,               
-                    email: req.body.email,
-                    cell: req.body.cell,
-                    profession: req.body.profession,
-                    address: req.body.address,
-                    address2: req.body.address2,
-                    city: req.body.city,
-                    state: req.body.state,
-                    zip: req.body.zip,
-                    username: req.body.username,
-                    pass: req.body.pass,
-                    created: new Date()
-                    });
-        
-                user.save()
-                    .then(data => {
-                        res.status(201).json({
-                            message: 'Data added successfully',
-                        });
-                    }).catch(err => {
-                        console.log(err);
-                    });
-            }
-        }).catch(err => {
-            res.status(400).json({
-                message: err,
-            });
-        })
-
-        
-	}
-});
+router.post('/user', conRegister.addUpdateUser);
 
 module.exports = router;
